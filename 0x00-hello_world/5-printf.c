a2cfd3ca4660376f10e82bfceb48edf12d1dcc2a
Benoo123 <marvelousben00@gmail.com>
2022-09-08 21:21:56 -0700

    My task2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 
 /**
- *  * main - Entryy point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main  - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "programmingis like building a mutilingual puzzle
+ * Return: 0
+ */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
