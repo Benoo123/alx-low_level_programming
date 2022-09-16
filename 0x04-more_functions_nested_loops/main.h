e7206e8515ab9da98063e380e9500c86476da4df
Benoo123 <marvelousben00@gmail.com>
2022-09-15 17:36:12 -0700

    My task

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main -header files for protypes
+ * void protypes( void)
+ * int protypes (int)
+ */
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(iny size);
+void print_number(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+
+#endif /* MAIN.H*
