1dae9418bfbab6385f5e7025819180c943c1ea90
Benoo123 <marvelousben00@gmail.com>
2022-09-19 12:54:59 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * pits_half - A function that prints second half of a string
+ * @str: The string to be printed
+ * Return: Always void (success)
+ */
+
+void puts_half(char *str)
+{
+	int len,
+	    i;
+
+	len = strlen(str);
+
+	if(len % 2 == 0)
+		i = len / 2;
+	else
+		i = (len + 1) / 2;
+
+	while (i < len)
+	{
+		putchar(str[i]);
+		i++;
+	};
+	putchar('\n')
+}
